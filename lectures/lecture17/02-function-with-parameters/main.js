535f34628c9557dc5f0fbb5399bf6d52faa7073a
Ben <tbengibson@gmail.com>
2023-03-22 15:46:14 -0700

    lecture 17

--- lectures/lecture17/02-function-with-parameters/main.js
+++ lectures/lecture17/02-function-with-parameters/main.js
@@ -1,1 +1,30 @@
 // make a function:
+function makeRed() {
+    document.querySelector('body').style.backgroundColor = 'red'
+}
+
+function makeBlue() {
+    document.querySelector('body').style.backgroundColor = 'blue'
+}
+
+ function makeGreen() {
+    document.querySelector('body').style.backgroundColor = 'green' 
+ } 
+
+ function makeOrange() {
+    document.querySelector('body').style.backgroundColor = 'Orange' 
+ } 
+
+function makePurple() {
+    document.querySelector('body').style.backgroundColor = 'Purple'
+}
+
+function makePink() {
+    document.querySelector('body').style.backgroundColor = 'Pink'
+}
+
+ function changeColor(bgcolor, textcolor){
+    document.querySelector ('body').style.backgroundColor = bgcolor;
+    document.querySelector('body').style.color = textcolor;
+}
+
